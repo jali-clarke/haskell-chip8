9103c5ffa8597c40c0dccd4584696c84d6707eb6
Jinnah Ali-Clarke <jinnah.ali-clarke@outlook.com>
2022-04-16 11:54:55 -0400

    add memory r/w

--- src/VMState.hs
+++ src/VMState.hs
@@ -3,6 +3,8 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module VMState
   ( VMExec,
+    readMemory,
+    writeMemory,
   )
 where
 
@@ -30,3 +32,12 @@ data Program n = Program {rom :: Vector.Vector n Word16, pc :: !(Finite n)}
 data VMState stackSize programSize = VMState {memory :: Memory, stack :: Stack stackSize, registers :: Registers, timers :: Timers, program :: Program programSize}
 
 newtype VMExec stackSize programSize a = VMExec (State.StateT (VMState stackSize programSize) (Except.ExceptT String IO) a) deriving (Functor, Applicative, Monad)
+
+readMemory :: Finite MemorySize -> VMExec stackSize programSize Word8
+readMemory memAddr = withMemory $ \stateMemory -> MVector.read (memData stateMemory) memAddr
+
+writeMemory :: Finite MemorySize -> Word8 -> VMExec stackSize programSize ()
+writeMemory memAddr byte = withMemory $ \stateMemory -> MVector.write (memData stateMemory) memAddr byte
+
+withMemory :: (Memory -> IO a) -> VMExec stackSize programSize a
+withMemory memoryAction = VMExec $ State.gets memory >>= State.liftIO . memoryAction

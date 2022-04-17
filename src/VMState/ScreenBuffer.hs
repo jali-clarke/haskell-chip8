b2d52160209fc596fc135661414778ba398d3ee5
Jinnah Ali-Clarke <jinnah.ali-clarke@outlook.com>
2022-04-17 14:38:58 -0400

    add setPixel

--- src/VMState/ScreenBuffer.hs
+++ src/VMState/ScreenBuffer.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoStarIsType #-}
 {-# LANGUAGE TypeOperators #-}
@@ -6,16 +7,21 @@ module VMState.ScreenBuffer (
   ScreenBuffer,
   Action,
   runAction,
-  newScreenBuffer
+  newScreenBuffer,
+  setPixel,
 ) where
 
 import BaseTypes
 import qualified Control.Monad.Reader as MTL
 import Control.Monad.Primitive (PrimState)
+import Data.Finite (Finite)
+import qualified Data.Finite as Finite
 import qualified Data.Vector.Unboxed.Mutable.Sized as SizedMVector
-import GHC.TypeNats (type (*))
+import GHC.TypeNats (type (+), type (*))
 
 type ScreenBufferSize = ScreenWidth * ScreenHeight
+
+type ScreenBufferAddress = Finite ScreenBufferSize
 
 type ScreenBufferData = SizedMVector.MVector ScreenBufferSize (PrimState IO) Bool
 
@@ -28,3 +34,28 @@ runAction (Action action) buffer = MTL.runReaderT action buffer
 
 newScreenBuffer :: IO ScreenBuffer
 newScreenBuffer = fmap ScreenBuffer SizedMVector.unsafeNew
+
+setPixel :: ScreenX -> ScreenY -> Bool -> Action Bool
+setPixel screenX screenY pixel = do
+  currentPixel <- getPixel screenX screenY
+  -- (/=) is xor
+  let newPixel = currentPixel /= pixel
+  Action $ do
+    ScreenBuffer bufferData <- MTL.ask
+    MTL.liftIO $ SizedMVector.write bufferData (toBufferAddress screenX screenY) newPixel
+  -- if the pixel was flipped from on to off, we have a collision
+  pure $ currentPixel && not newPixel
+
+getPixel :: ScreenX -> ScreenY -> Action Bool
+getPixel screenX screenY =
+  Action $ do
+    ScreenBuffer bufferData <- MTL.ask
+    MTL.liftIO $ SizedMVector.read bufferData (toBufferAddress screenX screenY)
+
+toBufferAddress :: ScreenX -> ScreenY -> ScreenBufferAddress
+toBufferAddress screenX screenY =
+  Finite.finite $
+    Finite.getFinite screenY * Finite.getFinite width + Finite.getFinite screenX
+
+width :: Finite (ScreenWidth + 1)
+width = Finite.finite 64
